125e1df81a2918708becd0e6c168e3831088e671
Yuragranovsky <yuragranovsky@gmail.com>
2023-09-21 19:36:53 +0300

    HW #32 Copy Files Performance Test

--- src/telran/io/FilesCopy.java
+++ src/telran/io/FilesCopy.java
@@ -1,10 +1,14 @@
 package telran.io;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
 public class FilesCopy implements CopyFile {
 
 	@Override
 	public void copy(String pathToSource, String pathToDestination) throws Exception {
-		// TODO Auto-generated method stub
+		Files.copy(Path.of(pathToSource), Path.of(pathToDestination), StandardCopyOption.REPLACE_EXISTING);
 
 	}
 
