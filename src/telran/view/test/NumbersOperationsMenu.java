6aa6a0aa25e86e13050323e71d1f9ddb728bcb23
Yuragranovsky <yuragranovsky@gmail.com>
2023-10-17 17:32:56 +0300

    HW #36 solution

---
+++ src/telran/view/test/NumbersOperationsMenu.java
@@ -0,0 +1,37 @@
+package telran.view.test;
+
+import java.util.function.BinaryOperator;
+
+import telran.view.*;
+
+
+public class NumbersOperationsMenu {
+	static String name;
+public static Item getOperationsItem(String name) {
+	NumbersOperationsMenu.name = name;
+	return Item.of(name, NumbersOperationsMenu::performMethod);
+	
+	
+	
+}
+static void twoNumbersAction(InputOutput io,
+		BinaryOperator<Double> operator) {
+	double firstNumber = io.readDouble("Enter first number", "no number");
+	double secondNumber = io.readDouble("Enter second number","no number");
+	io.writeObjectLine(operator.apply(firstNumber, secondNumber));
+}
+
+static void performMethod(InputOutput io1) {
+	Item [] items = {
+			Item.of("Add two numbers",
+					io -> twoNumbersAction(io, (a,b) -> a + b)),
+			Item.of("Subtract two numbers", io -> twoNumbersAction(io, (a,b) -> a - b)),
+			Item.of("Divide two numbers", io -> twoNumbersAction(io, (a,b) -> a / b)),
+			Item.of("Multiply two numbers", io -> twoNumbersAction(io, (a,b) -> a * b)),
+			
+			Item.exit()
+		};
+			Menu menu = new Menu(name, items);
+			menu.perform(io1);
+}
+}
