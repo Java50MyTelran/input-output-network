8c15a41aa2418241cf70c02caa11f42c97c25cc4
Yuragranovsky <yuragranovsky@gmail.com>
2023-11-28 19:05:39 +0200

    fix bug of connecting on writeObject

--- src/telran/net/TcpClientHandler.java
+++ src/telran/net/TcpClientHandler.java
@@ -2,35 +2,40 @@ package telran.net;
 
 import java.io.*;
 import java.net.*;
+
 public class TcpClientHandler implements Closeable, NetworkHandler {
-	 Socket socket;
-	 ObjectOutputStream writer;
-	 ObjectInputStream reader;
-	 String host;
-	 int port;
-	 private boolean flConnected = false;
-	 public TcpClientHandler(String host, int port) throws Exception {
-		 this.host = host;
-		 this.port = port;
-		
-	 }
+	Socket socket;
+	ObjectOutputStream writer;
+	ObjectInputStream reader;
+	String host;
+	int port;
+	private boolean flConnected = false;
+
+	public TcpClientHandler(String host, int port) throws Exception {
+		this.host = host;
+		this.port = port;
+
+	}
+
 	private void connect() {
 		try {
 			socket = new Socket(host, port);
 			writer = new ObjectOutputStream(socket.getOutputStream());
-			 reader = new ObjectInputStream(socket.getInputStream());
-			 flConnected = true;
+			reader = new ObjectInputStream(socket.getInputStream());
+			flConnected = true;
 		} catch (Exception e) {
 			flConnected = false;
 			throw new RuntimeException("server is unavailable, please repeat request later on");
 		}
-		
+
 	}
+
 	@Override
 	public void close() throws IOException {
 		socket.close();
 
 	}
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public <T> T send(String requestType, Serializable requestData) {
@@ -38,31 +43,29 @@ public class TcpClientHandler implements Closeable, NetworkHandler {
 		Response response = null;
 		boolean running = false;
 		try {
-			if(!flConnected) {
+			if (!flConnected) {
 				connect();
 			}
 			do {
 				running = false;
-				writer.writeObject(request);
 				try {
+					writer.writeObject(request);
+
 					response = (Response) reader.readObject();
 				} catch (SocketException e) {
 					connect();
 					running = true;
-				} 
+				}
 			} while (running);
-			if(response.code() != ResponseCode.OK) {
+			if (response.code() != ResponseCode.OK) {
 				throw new Exception(response.code() + ": " + response.responseData().toString());
 			}
-			
-			
-		}
-		catch (Exception e) {
+
+		} catch (Exception e) {
 			throw new RuntimeException(e.getMessage());
 		}
 		return (T) response.responseData();
-		
+
 	}
-	
 
 }
