08aaade95947b6ead026f73bd03e96c5cd955d4a
Yuragranovsky <yuragranovsky@gmail.com>
2023-11-30 20:10:11 +0200

    Update: removes switch from CompanyProtocol

--- src/telran/employees/CompanyProtocol.java
+++ src/telran/employees/CompanyProtocol.java
@@ -1,6 +1,7 @@
 package telran.employees;
 
 import java.io.Serializable;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 import telran.employees.dto.*;
@@ -9,7 +10,7 @@ import telran.net.ApplProtocol;
 import telran.net.Request;
 import telran.net.Response;
 import telran.net.ResponseCode;
-
+@SuppressWarnings("unused")
 public class CompanyProtocol implements ApplProtocol {
 	private Company company;
 
@@ -23,32 +24,24 @@ public class CompanyProtocol implements ApplProtocol {
 		String requestType = request.requestType();
 		Response response = null;
 		Serializable responseData = 0;
-		Integer defaultValue = Integer.MAX_VALUE;
+
 		try {
-			responseData = switch (requestType) {
-			case "employee/add" -> employee_add(requestData);
-			case "employee/get" -> employee_get(requestData);
-			case "employees/all" -> employees_all(requestData);
-			case "employee/salary/update" -> employee_salary_update(requestData);
-			case "employee/remove" -> employee_remove(requestData);
-			case "employees/department/salary/distribution" -> employees_department_salary_distribution(requestData);
-			case "employees/salary/distribution" -> employees_salary_distribution(requestData);
-			case "employees/department/get" -> employees_department_get(requestData);
-			case "employees/salary/get" -> employees_salary_get(requestData);
-			case "employees/age/get" -> employees_age_get(requestData);
-			case "employee/department/update" -> employees_department_update(requestData);
+			String methodName = requestType.replaceAll("/", "_");
+			Method method = getClass().getDeclaredMethod(methodName, Serializable.class);
+			responseData = (Serializable) method.invoke(this, requestData);
+			response = new Response(ResponseCode.OK, responseData);
 			
-			default -> defaultValue;
-			};
-			response = responseData == defaultValue ? new Response(ResponseCode.WRONG_TYPE, requestType)
-					: new Response(ResponseCode.OK, responseData);
-		} catch (Exception e) {
+		}catch (NoSuchMethodException e) {
+			response = new Response(ResponseCode.WRONG_TYPE, requestType);
+		}
+		catch (Exception e) {
 			response = new Response(ResponseCode.WRONG_DATA, e.getMessage());
 		}
 
 		return response;
 	}
 
+	
 	private Serializable employees_department_update(Serializable requestData) {
 		UpdateDepartmentData data = (UpdateDepartmentData) requestData;
 		return company.updateDepartment(data.id(), data.newDepartment());
